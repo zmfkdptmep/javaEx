a1b36754cc6240fd71b46db232044bc991c5c659
user <user@DESKTOP-2PGRMF4>
2023-03-17 11:33:48 +0900

    깃허브 연습용 데스요

---
+++ gitPractice/src/com/jungang/Hellohi.java
@@ -0,0 +1,25 @@
+package com.jungang;
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+
+
+public class Hellohi {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList<String>();
+		
+		al.add("1");
+		al.add("2");
+		al.add("3");
+		al.add("4");
+		al.add("5");
+		al.add("6");
+		al.add("7");
+		
+		
+		
+	}
+
+}
