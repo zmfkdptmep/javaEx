73c2db34323b4c7eb147d6ae245773aabf5fba12
user <user@DESKTOP-2PGRMF4>
2023-03-24 12:20:00 +0900

    ㅁㄴㅇ

--- ForPractice/src/com/jungang/ForEx1.java
+++ ForPractice/src/com/jungang/ForEx1.java
@@ -3,5 +3,13 @@ package com.jungang;
 public class ForEx1 {
 	public static void main(String[] args) {
 		
+		// 구구단 2단
+		// for(초기식;조건식;증감식)
+		int dan = 2;
+		
+		for(int i =1;i<10;i++) {
+			System.out.printf("%dX%d = %d\n",dan,i,dan*i);
+		}
+		
 	}
 }
